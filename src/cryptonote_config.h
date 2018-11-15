597ae7ae29567449625331524cf076062e0b5987
Reliefs <brominercom@gmail.com>
2018-11-16 00:04:18 +0100

    new seeds

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -160,11 +160,11 @@ namespace config
   uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 18;
   uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 19;
   uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 42;
-  uint16_t const P2P_DEFAULT_PORT = 2794;
+  uint16_t const P2P_DEFAULT_PORT = 2793;
   uint16_t const RPC_DEFAULT_PORT = 2794;
   uint16_t const ZMQ_RPC_DEFAULT_PORT = 18082;
   boost::uuids::uuid const NETWORK_ID = { {
-      0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
+      0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x17, 0x61, 0x41, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
     } }; // Bender's nightmare
   std::string const GENESIS_TX = "";
   uint32_t const GENESIS_NONCE = 10000;
@@ -178,9 +178,9 @@ namespace config
     uint16_t const RPC_DEFAULT_PORT = 2794;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 28082;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x41 , 0x04, 0x61, 0x17, 0x31, 0x00, 0x82, 0xA1, 0x16, 0xA1, 0x10
+      0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x17, 0x61, 0x41, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
+    std::string const GENESIS_TX = "";
     uint32_t const GENESIS_NONCE = 10001;
   }
 
